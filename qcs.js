3ab0cd30a9bb0e20b689fcff06f9162ae07b4a21
萝卜 <106028210+lu0b0@users.noreply.github.com>
2023-01-12 18:25:47 +0800

    Update qcs.js

--- qcs.js
+++ qcs.js
@@ -7,6 +7,7 @@
 抓包域名 ：mystore-01api.watsonsvip.com.cn
 export js名+Authorization="Bearer xxxxxxx"
 export js名+unionId="xxxxxxx"
+不清楚变量名运行脚本会提示
 支持圈X,青龙等。。
 重写：https://mystore-01api.watsonsvip.com.cn/saas-gateway/api/agg-trade/v1/signIn  url script-request-headers https://raw.githubusercontent.com/lu0b0/Script/main/qcs.js
 cron: 5 9 * * *
